6cbde07a2eb822933ee2c336f76b21731075aa5a
AGodinez <pistilloagodinez@gmail.com>
2015-05-17 15:21:35 -0600

    Repare bug #3

--- application/views/home/section-gallery.php
+++ application/views/home/section-gallery.php
@@ -1,9 +1,9 @@
 <div id="galery" class="pad-section">
 	<div class="container">
-		<div class="row">			
+		<div class="row">
 			<div class="col-lg-12 is-center">
 				<h1 class="text-head">Galer&iacute;a de Im&aacute;genes</h1>
-			</div>
+			</div><br>
 			<div id="blueimp-gallery" class="blueimp-gallery">
 				<div class="slides"></div>
 				<h3 class="title"></h3>
@@ -14,59 +14,49 @@
 			<ol class="indicator"></ol>
 		</div>
 		<div id="links">
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/1.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/1.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/2.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/2.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/3.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/3.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/4.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/4.jpg" alt="">
 				</a>
 			</div>
-			
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/1.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/1.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/2.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/2.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/3.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/3.jpg" alt="">
 				</a>
 			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
+			<div class="col-lg-3 col-sm-4 col-xs-12 thumb">
 				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/4.jpg">
 					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/4.jpg" alt="">
 				</a>
 			</div>
-			
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
-				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/1.jpg">
-					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/1.jpg" alt="">
-				</a>
-			</div>
-			<div class="col-lg-3 col-sm-4 col-xs-6 thumb">
-				<a class="thumbnail" href="<?= base_url()?>public/imgs/thumbs/2.jpg">
-					<img class="img-responsive" src="<?= base_url()?>public/imgs/thumbs/2.jpg" alt="">
-				</a>
-			</div>
+
 		</div>
-	</div>	
+	</div>
 </div>
 </div>
